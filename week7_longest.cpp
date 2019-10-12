75f294bdb5e83e91c21fcb9c3b1191705ea45192
shwetamali <53892753+shwetamali@users.noreply.github.com>
2019-10-12 09:48:38 +0530

    Create week7_longest.cpp

---
+++ week7_longest.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include<time.h>
+using namespace std;
+int main ( )
+{
+    clock_t start,end;
+    double d;
+    start=clock();
+    string s1, s2;
+    cout << "Enter the first string: ";
+    cin >> s1;
+    cout << "Enter the second string: ";
+    cin >> s2;
+    const size_t l1 = s1.length ( ), l2 = s2.length ( );
+    string subsequence = "";
+    for ( size_t start = 0; start < l1 && ( l1 - start ) > subsequence.length ( ); ++start )
+    {
+        string temp = "";
+        size_t x, pos = 0;
+        for ( size_t i = start; i < l1; ++i )
+        {
+            x = s2.find ( s1[i], pos );
+            if ( x != -1 )
+            {
+                temp += s1[i];
+                pos = x;
+            }
+        }
+        if ( temp.length ( ) > subsequence.length ( ) )
+            subsequence = temp;
+    }
+    cout << "Longest Subsequence: " << subsequence << "\nLength: " << subsequence.length ( ) << "\n";
+    cin.ignore ( );
+    end=clock();
+    d=d+(double)(end-start)/CLOCKS_PER_SEC;
+    cout<<"Time complexity:"<<d;
+    return 0;
+}
